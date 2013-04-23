eeb38f67c53d5836aa6b403af0519bb96f81d5a9
Shane Elliott <shane@pukunui.com>
2013-04-23 08:16:00 +0800

    Adding navigation link

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Sample report
+ *
+ * Navigation and settings
+ *
+ * @package    report
+ * @subpackage sample
+ * @author     Shane Elliott <shane@pukunui.com>
+ * @copyright  2013 #mootnz13
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+defined('MOODLE_INTERNAL') || die();
+
+$ADMIN->add('reports', new admin_externalpage('reportsample', get_string('pluginname', 'report_sample'), "$CFG->wwwroot/report/sample/index.php",'report/sample:view'));
+
+// No report settings.
+$settings = null;
--- version.php
+++ version.php
@@ -13,7 +13,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013010100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013010101;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'report_sample';   // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_ALPHA;
