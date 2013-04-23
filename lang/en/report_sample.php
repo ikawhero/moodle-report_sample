5d1c0ea8fbaa08d8c2d48ddf90bda05571b5499b
Shane Elliott <shane@pukunui.com>
2013-04-23 08:12:42 +0800

    Adding internal check

--- lang/en/report_sample.php
+++ lang/en/report_sample.php
@@ -11,5 +11,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 
+defined('MOODLE_INTERNAL') || die();
+
 $string['title'] = 'Sample Report';
 $string['pluginname'] = 'Sample Report';
